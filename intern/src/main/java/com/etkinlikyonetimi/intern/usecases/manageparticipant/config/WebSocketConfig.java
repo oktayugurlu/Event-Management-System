51a95ead16512619297efbebe0f255268976589d
Oktay UGURLU <32527753+oktayugurlu@users.noreply.github.com>
2020-12-13 11:23:26 +0300

    Update WebSocketConfig.java

--- intern/src/main/java/com/etkinlikyonetimi/intern/usecases/manageparticipant/config/WebSocketConfig.java
+++ intern/src/main/java/com/etkinlikyonetimi/intern/usecases/manageparticipant/config/WebSocketConfig.java
@@ -12,18 +12,18 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        //Users send data to this url and the message is return to "/app/notify"
-        registry.addEndpoint("/sendNotification").setAllowedOrigins("*").withSockJS();
-    }
-
-    @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         //Registered users can subscribe this url to reach the messages. componentDidMount should
         //be used as app/notify
         config.enableSimpleBroker("/notify");
         config.setApplicationDestinationPrefixes("/app");   // Enables a simple in-memory broker
 
+    }
+      
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //Users send data to this url and the message is return to "/app/notify"
+        registry.addEndpoint("/sendNotification").setAllowedOrigins("*").withSockJS();
     }
 
 
